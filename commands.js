8064180779ad230da3fe58c7b9f4c39926f5e952
Tetrabyte <Byte@Tetranite.com>
2017-09-23 21:01:01 -0600

    Adding command templates for later

--- commands.js
+++ commands.js
@@ -12,4 +12,84 @@ cmds.ping = {
     }
 };
 
+cmds.play = {
+    name: `play`,
+    help: `Play some music using a youtube link or search query.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Play Command //
+    }
+};
+
+cmds.stop = {
+    name: `stop`,
+    help: `Make the bot stop playing music and disconnect from the channel.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Stop command //
+    }
+};
+
+cmds.volume = {
+    name: `volume`,
+    help: `Change the current volume that the music is playing on.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Volume Command //
+    }
+};
+
+cmds.pause = {
+    name: `pause`,
+    help: `Pause the current playing song.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Pause Command //
+    }
+};
+
+cmds.resume = {
+    name: `resume`,
+    help: `Resume the music playing if you paused it.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Resume Command //
+    }
+};
+
+cmds.queue = {
+    name: `queue`,
+    help: `Get a list of the songs currently in the queue.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Queue Command //
+    }
+};
+
+cmds.shuffle = {
+    name: `shuffle`,
+    help: `Shuffle the current queue.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Shuffle Command //
+    }
+};
+
+cmds.remove = {
+    name: `remove`,
+    help: `Remove a song from the queue.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Remove Command //
+    }
+};
+
+cmds.repeat = {
+    name: `repeat`,
+    help: `Make the bot repeat the current song until you turn it off.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Repeat Command //
+    }
+};
+
+cmds.help = {
+    name: `help`,
+    help: `Returns with a list of commands for the bot.`,
+    trigger: ({ client, msg, params, raw, clean }) => {
+        // Help Command //
+    }
+};
+
 module.exports = cmds;
